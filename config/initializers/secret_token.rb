f8204b02dbb26ca5bdb716ef46d6a6189e8a73d5
opsb <oliver@opsb.co.uk>
2011-07-31 23:25:28 +0100

    Add rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gyro::Application.config.secret_token = '09ad44142a96d48ed38e29d19c7289576bc8fdef32c1c4996af2e8289e59e6975f3a3ae342835ba77368903a62fe3a3cb9ee0a33fd7870b67088b5e345f96346'
